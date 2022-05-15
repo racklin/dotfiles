39dd5b7a7b4516d468a3100988ac88dbf1da7e66
Rack Lin <racklin@gmail.com>
2022-05-15 20:52:48 +0800

    feat(editorconfig): add editorconfig and using php-cli from container

--- zsh/dot-config-osx.zsh
+++ zsh/dot-config-osx.zsh
@@ -1,10 +1,9 @@
-# Add HomeBrew PATH
-export PATH=/opt/homebrew/bin:$PATH
-
 # EXA
 [ -x /opt/homebrew/bin/exa ] && alias ll="exa -l -g --icons"
 
-# PHP
-export PATH="/opt/homebrew/opt/php/bin:$PATH"
-export PATH="/opt/homebrew/opt/php/sbin:$PATH"
-
+# PHP in container
+[ -x /usr/local/bin/docker ] && alias composer="docker run --rm --interactive --tty --volume \$PWD:/app --volume \$HOME/.composer:/tmp composer:latest composer" 
+[ -x /usr/local/bin/docker ] && alias php="docker run --rm --interactive --tty --volume \$PWD:/app --volume /Users:/Users composer:latest php" 
+[ -x /usr/local/bin/docker ] && alias php8="docker run --rm --interactive --tty --volume \$PWD:/app composer:latest php" 
+[ -x /usr/local/bin/docker ] && alias php7="docker run --rm --interactive --tty --volume \$PWD:/app --workdir /app php:7.4-cli php" 
+[ -x /usr/local/bin/docker ] && alias laravel="docker run --rm --interactive --tty --volume \$PWD:/app --volume \$HOME/.composer:/tmp composer:latest /tmp/vendor/bin/laravel" 
